c68cd4524dc8f3e77afc072662500ca53bc971ee
hqdat809 <dathq@eupgroup.net>
2024-06-07 17:33:46 +0700

    fix docker 5

--- src/main/java/com/example/HandBook/FirebaseConfig.java
+++ src/main/java/com/example/HandBook/FirebaseConfig.java
@@ -5,21 +5,25 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("target/classes/firebase-service-account.json");
+            ClassPathResource serviceAccountResource = new ClassPathResource("firebase-service-account.json");
+            InputStream serviceAccountStream  = serviceAccountResource.getInputStream();
+//            FileInputStream serviceAccount =
+//                    new FileInputStream("target/classes/firebase-service-account.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
                     .setDatabaseUrl("https://sotay-74bbe-default-rtdb.firebaseio.com")
                     .build();
 
